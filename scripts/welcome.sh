7d2ddf1232f9e44014747b41e1ca4b5ea3d32839
nandanVasistaBH29 <108749491+nandanVasistaBH29@users.noreply.github.com>
2026-06-19 11:38:31 +0530

    Create welcome.sh

---
+++ scripts/welcome.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "===== GitHub Actions Info ====="
+echo "Actor      : $GITHUB_ACTOR"
+echo "Repository : $GITHUB_REPOSITORY"
+echo "Branch     : $GITHUB_REF_NAME"
+echo "Runner OS  : $RUNNER_OS"
+echo "Workspace  : $GITHUB_WORKSPACE"
+echo "==============================="
